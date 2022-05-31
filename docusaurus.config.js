e0ea0c79417a2b6b5ec6e4b65e10f434c80b10e0
Hugo Oliveira <holiveira@ciandt.com>
2022-05-31 20:56:57 -0300

    feat: update name of the github repository

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/HugoOliveiraThor',
               },
             ],
           },
